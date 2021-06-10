9dd0948e7a4c87740a71da2447f40a5d0333bf0b
tanyapole <tanusha2406@gmail.com>
2021-06-10 07:50:54 +0000

    add fn for loading pytorch model with saved weights

--- model_creating.py
+++ model_creating.py
@@ -1,8 +1,19 @@
 from torchvision.models import resnet34
 import torch, torch.nn as nn
+from pathlib import Path
 
 
 def create_model(num_classes):
     model = resnet34(pretrained=True)
     model.fc = nn.Linear(model.fc.in_features, num_classes, bias=(model.fc.bias is None))
     return model
+
+def load_model(fldr, num_classes, device=None):
+    m = create_model(num_classes)
+    src_fldr = Path('.') #'../derm-dis-morph')
+    pt = list((src_fldr/fldr).iterdir())[0]
+    m.load_state_dict(torch.load(pt))
+    m = m.eval()
+    if device is not None:
+        m = m.to(device)
+    return m
